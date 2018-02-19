d1eca55b8940f8a7b2b5d9d7289c14bae53cdd8c
ifilot <ivo@zuidstijl.nl>
2018-02-19 08:03:07 +0100

    Adding permutations

--- matrix-alignment.py
+++ matrix-alignment.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # build two adjacency matrices
 V1 = np.matrix('0 1 0 0 0 ; 1 0 1 0 0 ; 0 1 0 1 1 ; 0 0 1 0 0 ; 0 0 1 0 0')
-V2 = np.matrix('0 1 0 0 0 ; 1 0 1 0 0 ; 0 1 0 1 1 ; 0 0 1 0 0 ; 0 0 1 0 0')
+V2 = np.matrix('0 0 1 0 0 ; 0 0 1 1 1 ; 1 1 0 0 0 ; 0 1 0 0 0 ; 0 1 0 0 0')
 
 # define matrices
 matsize = V1.shape[0]
@@ -30,12 +30,50 @@ for i in range(0, matsize):			# loop over V1
 				v += 1
 		u += 1
 
+# use both network information as well as "diffusion" information; use a power method
 alpha = 0.6
 for i in range(0,20):
 	prod = alpha * np.dot(A,b) + (1 - alpha) * E
 	b = prod / np.linalg.norm(prod)
 
+# construct B in matrix form and output result
+bmatrix = np.zeros([matsize, matsize])
 for i in range(0,matsize):
 	for j in range(0,matsize):
-		print b[i * 5 + j],
-	print
+		bmatrix[i,j] = b[i * 5 + j]
+print bmatrix
+
+# construct index lists
+rr = range(0, matsize)
+cc = range(0, matsize)
+
+# build empty permutation list
+perm = np.zeros([matsize, 1])
+
+# find all permutations
+for i in range(0, matsize):		# find 5 permutations
+	maxsize = -1
+	ir = -1
+	ic = -1
+	for r in rr:
+		for c in cc:
+			if bmatrix[r,c] > maxsize:
+				maxsize = bmatrix[r,c]
+				ir = r
+				ic = c
+	# store index
+	perm[ir] = ic
+	# remove highest indices from list
+	rr.remove(ir)
+	cc.remove(ic)
+
+print perm
+
+# construct permutation matrix from these permutations
+P = np.zeros([matsize, matsize])
+for idx,val in enumerate(perm):
+	P[idx,int(val)] = 1
+
+# verify the alignment procedure
+print V1
+print P * V2 * P.transpose()
